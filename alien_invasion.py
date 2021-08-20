3ba5dd1008cab1220fc6433db6c19bf6159ee017
JKK86 <j.kurowski86@wp.pl>
2021-08-20 18:26:35 +0200

    code refactor

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,10 +93,14 @@ class AlienInvasion:
 
         # Utworzenie pierwszego rzędu floty obcych
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien_x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien_x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien_x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien_x
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Uaktualnianie obrazów na ekranie i przejścia do nowego ekranu"""
