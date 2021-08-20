0bc9b3fbca7ff03dc644e41d8fb39ab6631cad86
JKK86 <j.kurowski86@wp.pl>
2021-08-20 18:42:54 +0200

    create alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,19 +87,27 @@ class AlienInvasion:
         # Utworzenie obcego i obliczenie liczby obcych, którzy zmieszczą się w jednym rzędzie
         # Odległość między obcymi przyjęto równą szerokości obcego
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - 2 * alien_width
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Utworzenie pierwszego rzędu floty obcych
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Obliczenie liczby rzędów obcych mieszczących się na ekranie
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - 3 * alien_height - ship_height
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        # Utworzenie floty obcych
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        """Utworzenie obcego i umieszczenie go w rzędzie"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien_x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien_x
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
