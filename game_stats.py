9af5dd59c42d616ac352514d6821c4e051aa114b
JKK86 <j.kurowski86@wp.pl>
2021-09-25 23:59:35 +0200

    add high score

--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,7 @@ class GameStats:
         self.reset_stats()
         # Uruchomienie gry w stanie nieaktywnym
         self.game_active = False
+        self.high_score = 0
 
     def reset_stats(self):
         """Inicjalizacja danych statystycznych zmieniających się w czasie gry"""
--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,7 @@ class Scoreboard:
 
         # Przygotowanie początkowch obrazów z punktacją
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Przekształcenie punktacji na wygenerowany obraz"""
@@ -28,6 +29,18 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Przekształcenie najlepszego wyniku w grze na wygenerowany obraz"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+
+        # Wyświetlanie najlepszego wyniku na środku, przy górnej krawędzi ekranu
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """Wyświetlanie punktacji na ekranie"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
